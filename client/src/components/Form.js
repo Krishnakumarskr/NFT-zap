9893cc6d1d786d2f3e642dbbc71e6a1d17ff000f
Krishnakumarskr <krishna291098@gmail.com>
2021-09-16 20:25:48 +0530

    added approve and createNew contract function

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,13 +1,5 @@
-import React, { useEffect } from "react";
-import {useState} from 'react';
-import AtomicSwapContract from "./contracts/HashedTimelockERC721.json";
-//import SimpleStorageContract from './contracts/SimpleStorage.json';
-import getWeb3 from "./getWeb3";
-import Navbar from './components/Navbar';
-import AppContext from './components/AppContext';
-import Form from './components/Form';
-
-import "./App.css";
+import React, { useContext } from "react";
+import AppContext  from "./AppContext";
 
 const nftAbi = [
     {
@@ -287,99 +279,41 @@ const nftAbi = [
       "stateMutability": "nonpayable",
       "type": "function"
     }
-]
+  ]
 
-const App = () => {
-
-    const [accounts, setAccounts] = useState(null);
-    const [instances, setInstances] = useState({
-        web3: null,
-        contract: null
-    });
-
-    const SwapContractAddress = '0x09AfBCE734A23C1eA5B780B3339eb9308eccBD81';
-      
-    //Retrive from local storage
-    useEffect(() => {
-        const json = localStorage.getItem("accounts");
-        const accounts = JSON.parse(json);
-        if (accounts !== null) {
-            setAccounts(accounts);
-            (async () => {
-                const web3 = await getWeb3();
-                const instance = new web3.eth.Contract(
-                    AtomicSwapContract.abi,
-                    SwapContractAddress
-                );
-                
-                setInstances({web3, contract:instance});
-            })();
+const Form = ({swapAddress}) => {
+    const globalContext = useContext(AppContext);
+    
+    const getContractFunc = async () => {
+        const account = globalContext.accounts[0];
+        const data = {
+            receiver: '0xb4e912C0ED3B356af88Ee2587250875d4676Ca02',
+            hashLock: '0x37a57542613fc4088aefd44a8e4378c8cfae4e5206',
+            tokenContract: '0xB2Ee5deE09268e2bDb02b67268F345E298164826',
+            tokenId: 1,
+            requestedContract: '0xB2Ee5deE09268e2bDb02b67268F345E298164826',
+            requestedId: 2
         }
-    }, []);
+        const { newContract } = globalContext.instances.contract.methods;
+        // console.log(newContract);
+        const nftInstance = new globalContext.instances.web3.eth.Contract(
+            nftAbi,
+            data.tokenContract
+        );
+        const res = await nftInstance.methods.approve(swapAddress, data.tokenId).send({from: account});
 
-    //Add to local storage
-    useEffect(() => {
-        const json = JSON.stringify(accounts);
-        localStorage.setItem('accounts', json);
-    }, [accounts]);
+        console.log(res);
+        //const { approve } = globalContext.instances.contract.methods;
+        //approve('0xb4e912C0ED3B356af88Ee2587250875d4676Ca02', '0')
+        const result = await newContract(data.receiver, data.hashLock, data.tokenContract, data.tokenId, data.requestedContract, data.requestedId).send({from: account});
+        console.log(result);
 
-    const globalStates = {
-        accounts,
-        instances
     }
-
-    console.log(globalStates);
-
-    const accountSignUp = async () => {
-        console.log('calling signup')
-        try {
-            // Get network provider and web3 instance.
-            
-            const web3 = await getWeb3();
-
-            // Use web3 to get the user's accounts.
-            const accounts = await web3.eth.getAccounts();
-
-            // Get the contract instance.
-            // const networkId = await web3.eth.net.getId();
-            // const deployedNetwork = SimpleStorageContract.networks[networkId];
-            const instance = new web3.eth.Contract(
-                AtomicSwapContract.abi,
-                '0x2C42919c1fcA38bdad17F7715F871a78E06c7ee4'
-                // SimpleStorageContract.abi,
-                // deployedNetwork && deployedNetwork.address,
-            );
-
-            const nftInstance = new web3.eth.Contract(
-                nftAbi,
-                '0x2C42919c1fcA38bdad17F7715F871a78E06c7ee4'
-            );
-
-            // Set web3, accounts, signup to the global state.
-            // setGlobalStates({web3, accounts, signUp: true, instance});
-            setAccounts(accounts);
-            setInstances({web3, contract:instance, nft: nftInstance});
-
-            // console.log(instance);
-            // console.log(web3);
-
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-            alert(
-                `Failed to load web3, accounts, or contract. Check console for details.`,
-            );
-            console.error(error);
-        }
-    }
-
     return (
-       <AppContext.Provider value={globalStates}>
-            <div className="App">
-                <Navbar signUp={accountSignUp} />
-                <Form swapAddress={SwapContractAddress}/>
-            </div>
-        </AppContext.Provider>
+        <button onClick={getContractFunc}>
+            Get contract
+        </button>
     );
 }
 
-export default App;
+export default Form;
